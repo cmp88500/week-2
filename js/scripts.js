632686a95c34bec4e033e0980b588ebc8234bf36
Student <example@example.com>
2018-08-14 12:59:05 -0700

    Made folder for week 2 exercises

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// //1.1 Functions
+//
+// //1. Use alert()
+// alert ("Hello world")
+
+// //2. Use confirm
+// confirm ("Is this the question?")
+
+// //3. Use prompt
+// prompt ("Is this the question?")
+
+// //4. Save prompt response as a variable
+// var varOne = prompt("How are you?")
+// console.log(varOne)
+// var varTwo = confirm("Yes or no?")
+// console.log(varTwo)
+// var varThree = prompt("What did you eat?")
+// console.log(varThree)
+
+//5. Ask new question nad save response as new variable. Run 3 methods.
+var mathFour = (7.4853 + 8.3258).toFixed(2);
+var varFour = "Round to 2 decimal places: 7.4853 + 8.3258 =" + mathFour
+console.log(varFour)
+var mathFive = (748.53 + 632.58).toFixed();
+var varFive = "Round to nearest whole number: 748.53 + 632.58 =" + mathFive
+console.log(varFive)
+var mathSix = (153.6894).toPrecision(4);
+var varSix = "Use precision to get 153.6894 to 1 decimal place :" + mathSix
+console.log(varSix)
